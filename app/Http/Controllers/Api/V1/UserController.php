74b4d3a367ba2ec182492a9ea3268291955e88b9
Maga-Gazdiev <marmokanime@yandex.com>
2024-02-15 18:15:20 +0300

    Все что связано с User

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -10,68 +10,71 @@ use Spatie\Permission\Models\Role;
 use App\Models\User;
 use App\Models\Comment;
 use App\Models\Post;
+use App\Models\Book;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): \Illuminate\Http\JsonResponse
     {
-        $users = UserResource::collection(User::get());
+        $users = UserResource::collection(User::paginate(10));
         return response()->json($users);
     }
-
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id): \Illuminate\Http\JsonResponse
     {
-        $user = User::with('posts')->findOrFail($id);
-        $this->authorize('view', $user);
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['error' => 'Пользователь не найден'], 404);
+        }
 
         $userResource = new UserResource($user);
         return response()->json($userResource);
     }
 
-
-    public function update(UpdateUserRequest $request, $id)
+    public function update(UpdateUserRequest $request, $id): \Illuminate\Http\JsonResponse
     {
         $user = User::find($id);
 
         if (!$user) {
-            return response()->json(['error' => 'User not found'], 404);
+            return response()->json(['error' => 'Пользователь не найден'], 404);
         }
 
         $this->authorize('update', $user);
 
         $user->update([
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'password' => $request->has('password') ? Hash::make($request->input('password')) : $user->password,
+            'name' => $request->has('name') ? $request->input('name') : $user->name,
+            'email' => $request->has('email') ? $request->input('email') : $user->email,
+            'password' => $request->has('password') ? bcrypt($request->input('password')) : $user->password,
         ]);
 
-        return response()->json(['message' => 'User updated successfully']);
+        return response()->json(['message' => 'Данные пользователя успешно обновлены']);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy($id)
+    public function destroy($id): \Illuminate\Http\JsonResponse
     {
         $user = User::find($id);
 
         if (!$user) {
-            return response()->json(['error' => 'User not found'], 404);
+            return response()->json(['error' => 'Пользователь не найден'], 404);
         }
 
         $this->authorize('delete', $user);
 
         Comment::where('user_id', $id)->delete();
         Post::where('user_id', $id)->delete();
+        Book::where('user_id', $id)->delete();
         $user->delete();
 
-        return response()->json(['message' => 'User deleted successfully'], 204);
+        return response()->json(['message' => 'Пользователь успешно удален'], 200);
     }
 }
--- app/Http/Requests/User/UpdateUserRequest.php
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -19,13 +19,30 @@ class UpdateUserRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
-    public function rules()
-    {
-        $userId = $this->route('user');
-        return [
-            'name' => 'required|string',
-            'email' => 'required|email|unique:users,email,' . $userId, // Уникальность email, исключая текущего пользователя
-            'password' => 'required|string|min:5', 
-        ];
-    }
+     public function rules()
+     {
+         $userId = $this->route('user');
+         return [
+             'name' => 'string|min:5|max:50',
+             'email' => 'email|unique:users,email|min:10|max:50' . $userId,
+             'password' => 'string|min:4|max:40',
+         ];
+     }
+ 
+     public function messages(): array
+     {
+         return [
+             'name.string' => 'Можно использовать только символы',
+             'name.min' => 'Минимальное колличество символов должно быть 5',
+             'name.max' => 'Максимальное колличество символов должно быть 50',
+ 
+             'email.email' => 'Введите валидный Email',
+             'email.unique' => 'Это поле должо быть уникальным',
+             'email.min' => 'Минимальное колличество символов должно быть 10',
+             'email.max' => 'Максимальное колличество символов должно быть 50',
+ 
+             'password.min' => 'Минимальное колличество символов должно быть 4',
+             'password.max' => 'Максимальное колличество символов должно быть 40',
+         ];
+     }
 }
